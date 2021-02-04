3790f0cf45afb3a845ffb05e98dcdb62ba6205b4
MeetBudheliya <meetbudheliya04@gmail.com>
2021-02-04 15:16:05 +0530

    04/05/2021

--- CountryPickerDemo/ViewController.swift
+++ CountryPickerDemo/ViewController.swift
@@ -6,14 +6,26 @@
 //
 
 import UIKit
-
+import CountryPicker
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField: UITextField!
+    var countryPicker = CountryPicker()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.inputView = countryPicker
+        let locale = Locale.current
+        let code = (locale as NSLocale).object(forKey: NSLocale.Key.countryCode) as! String?
+        countryPicker.countryPickerDelegate = self
+        countryPicker.showPhoneNumbers = true
+        countryPicker.setCountry(code!)
     }
-
-
 }
-
+extension ViewController:CountryPickerDelegate{
+    
+    func countryPhoneCodePicker(_ picker: CountryPicker, didSelectCountryWithName name: String, countryCode: String, phoneCode: String, flag: UIImage) {
+        textField.text = phoneCode
+    }
+    
+    
+}
